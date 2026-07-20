000679bc622509e2d1c0f21d626be7024426028f
Facundo Pompa <pompafacundo@gmail.com>
2026-07-20 16:26:31 -0300

    menu interactivo con switch

--- src/fundamentos/prueba/Main.java
+++ src/fundamentos/prueba/Main.java
@@ -7,30 +7,48 @@ import fundamentos.prueba.util.ScannerUtils;
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "Cine Java";
     public static final String VERSION = "1.0.0";
+
+    public static final int AGREGAR = 1;
+    public static final int MOSTRAR_TODO = 2;
+    public static final int BUSCAR_POR_TITULO = 3;
+    public static final int ELIMINAR = 4;
+    public static final int SALIR = 5;
     public static void main(String[] args) {
         Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
-        String titulo = ScannerUtils.capturarTexto("Nombre del contenido");
-        String genero = ScannerUtils.capturarTexto("Genero del contenido");
-        int duracion = ScannerUtils.capturarNumero("Duración del contenido");
-        double calificacion = ScannerUtils.capturarDecimal("Calificación del contenido");
+        while (true) {
+            int opcionElegida = ScannerUtils.capturarNumero("""
+                    Ingrese una de las siguientes opciones: 
+                    1. Agregar contenido
+                    2. Mostrar todo
+                    3. Buscar por titulo
+                    4. Eliminar
+                    5. Salir
+                    """);
 
-        Pelicula pelicula = new Pelicula(titulo, duracion, genero, calificacion);
-        Pelicula pelicula2 = new Pelicula("El padrino", 220, "drama", 4.8);
 
-        plataforma.agregar(pelicula);
-        plataforma.agregar(pelicula2);
+            System.out.println("Opcion elegida: " + opcionElegida);
 
-        System.out.println("Numero de elementos en la plataforma: " + plataforma.getContenido().size());
+            switch (opcionElegida) {
+                case AGREGAR -> {
+                    String titulo = ScannerUtils.capturarTexto("Nombre del contenido");
+                    String genero = ScannerUtils.capturarTexto("Genero del contenido");
+                    int duracion = ScannerUtils.capturarNumero("Duración del contenido");
+                    double calificacion = ScannerUtils.capturarDecimal("Calificación del contenido");
 
-        plataforma.eliminar(pelicula2);
+                    plataforma.agregar(new Pelicula(titulo, duracion, genero, calificacion));
+                }
+                case MOSTRAR_TODO -> plataforma.mostrarTitulos();
+                case BUSCAR_POR_TITULO -> {
 
-        plataforma.mostrarTitulos();
+                }
+                case ELIMINAR -> {
 
-        Usuario usuario = new Usuario("Juan", "juan@gmail.com");
-        System.out.println(usuario.fechaRegistro);
+                }
+                case SALIR -> System.exit(0);
+            }
 
-        usuario.ver(pelicula);
+        }
     }
 }
